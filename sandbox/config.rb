57dde5be9938cb76fd1c5b6d05f511e89634d201
Scott Kellum <scott@scottkellum.com>
2012-06-11 08:07:08 -0400

    Including sassymath logic.

--- sandbox/config.rb
+++ sandbox/config.rb
@@ -1,29 +1,10 @@
 # Require any additional compass plugins here.
+require 'compass'
 require 'fraction'
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
-
+css_dir = 'css'
+sass_dir = 'sass'
 
 # Sass Script Proving Grounds
 # Fractions
@@ -87,16 +68,9 @@ module Sass::Script::Functions
     Sass::Script::Number.new(result)
   end
   # Constants
-  def pi
-    pi = Math::PI
-    Sass::Script::Number.new(pi)
-  end
-  def e
-    e = Math::E
-    Sass::Script::Number.new(e)
-  end
   def rand
-    Sass::Script::Number.new(4)
+    result = Random.rand(10)
+    Sass::Script::Number.new(result)
   end
   def golden_ratio()
     result = (1.0 / 2.0) + (Math.sqrt(5) / 2.0)
