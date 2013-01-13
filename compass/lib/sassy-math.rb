52e93037030d2d2ebee720cb98ac240a291a8b82
Scott Kellum <scott@scottkellum.com>
2013-01-12 22:52:44 -0500

    using the random function from harsh.

--- compass/lib/sassy-math.rb
+++ compass/lib/sassy-math.rb
@@ -48,8 +48,12 @@ module Sass::Script::Functions
     end 
     Sass::Script::Number.new(result)
   end
-  def random(max = Sass::Script::Number.new(100)) ## shamelessly taken from here: https://gist.github.com/1561650
-    Sass::Script::Number.new(rand(max.value), max.numerator_units, max.denominator_units)
+  def random(type = Sass::Script::String.new("number"), max = Sass::Script::Number.new(100)) ## https://gist.github.com/1561650 Chris E.
+    if type.value == "color"
+      Sass::Script::Color.new(:red => rand(255), :green => rand(255), :blue => rand(255))
+    else
+      Sass::Script::Number.new(rand(max.value), max.numerator_units, max.denominator_units)
+    end
   end
   def hypot(a, b)
     a = a.value.to_f
